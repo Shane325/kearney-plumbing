f182f0af951b504a41ee15e7539adf3e190de4de
Shane325 <Shane325@gmail.com>
2018-07-25 16:59:36 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Module dependencies
+ *
+ */
+let express = require('express');
+let app = express();
+let path = require('path');
+let favicon = require('serve-favicon');
+let bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+
