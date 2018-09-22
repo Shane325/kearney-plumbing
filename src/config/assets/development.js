5758fd7f6e058ef53ccfec53a009c9aa78ba630e
Shane325 <Shane325@gmail.com>
2018-09-22 10:47:18 -0700

    Added new home header and call to action.

--- src/config/assets/development.js
+++ src/config/assets/development.js
@@ -6,7 +6,10 @@ module.exports = {
       '/css/global.css',
       '/css/structure.css',
       '/css/be_style.css',
-      '/css/custom.css'
+      '/css/custom.css',
+      '/plugins/rs-plugin/css/settings.css',
+      'plugins/rs-plugin/css/layers.css',
+      'plugins/rs-plugin/css/navigation.css'
     ],
     js: [
       '/js/jquery-2.1.4.min.js',
@@ -16,16 +19,17 @@ module.exports = {
       '/js/animations/animations.js',
       '/js/email.js',
       '/js/scripts.js',
-      'plugins/rs-plugin/js/jquery.themepunch.tools.min.js',
-      'plugins/rs-plugin/js/jquery.themepunch.revolution.min.js',
-      'plugins/rs-plugin/js/extensions/revolution.extension.video.min.js',
-      'plugins/rs-plugin/js/extensions/revolution.extension.slideanims.min.js',
-      'plugins/rs-plugin/js/extensions/revolution.extension.actions.min.js',
-      'plugins/rs-plugin/js/extensions/revolution.extension.layeranimation.min.js',
-      'plugins/rs-plugin/js/extensions/revolution.extension.kenburn.min.js',
-      'plugins/rs-plugin/js/extensions/revolution.extension.navigation.min.js',
-      'plugins/rs-plugin/js/extensions/revolution.extension.migration.min.js',
-      'plugins/rs-plugin/js/extensions/revolution.extension.parallax.min.js'
+      '/plugins/rs-plugin/js/jquery.themepunch.tools.min.js',
+      '/plugins/rs-plugin/js/jquery.themepunch.revolution.min.js',
+      '/plugins/rs-plugin/js/extensions/revolution.extension.video.min.js',
+      '/plugins/rs-plugin/js/extensions/revolution.extension.slideanims.min.js',
+      '/plugins/rs-plugin/js/extensions/revolution.extension.actions.min.js',
+      '/plugins/rs-plugin/js/extensions/revolution.extension.layeranimation.min.js',
+      '/plugins/rs-plugin/js/extensions/revolution.extension.kenburn.min.js',
+      '/plugins/rs-plugin/js/extensions/revolution.extension.navigation.min.js',
+      '/plugins/rs-plugin/js/extensions/revolution.extension.migration.min.js',
+      '/plugins/rs-plugin/js/extensions/revolution.extension.parallax.min.js',
+      'plugins/rs-plugin/js/extensions/revolution.extension.carousel.min.js'
     ]
   }
 }
